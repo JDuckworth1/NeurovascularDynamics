68c6f7cd43afd9ebd1010de25385e4a2f4446bae
jaduckwo <jaduckwo@ucsd.edu>
2024-04-22 08:26:19 -0700

    Updated weighted aveage file

--- Statistics/WeightedAvg_SEM.m
+++ Statistics/WeightedAvg_SEM.m
@@ -1,18 +1,13 @@
 %WeightedAvg_SEM.m
-%Uses an effective degrees of freedom
 
 function [wtdavg,wtdSEM,wtdSD] = WeightedAvg_SEM(data,wts)
 
 wtdavg = sum(data.*wts)/sum(wts);
-
 n = numel(data);
 wtdx2 = sum(wts.*data.^2)/sum(wts);
 wtd2mom = wtdx2 - wtdavg^2;
-df_factor = (sum(wts)^2)/((sum(wts)^2) - sum(wts.^2));
-df_a = (sum(wts)^2);
-df_b = sum(wts.^2);
-wtdVar = wtd2mom*df_factor;
+wtdVar = wtd2mom*n/(n-1);
 wtdSD = sqrt(wtdVar);
-wtdSEM = sqrt(wtd2mom*(df_b/(df_a-df_b)));
+wtdSEM = wtdSD/sqrt(n);
 
 end
